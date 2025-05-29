556f737ca976d6ab919941f01893ba241695259e
Darrell Tan <darrelltwh@gmail.com>
2025-05-29 17:38:56 +0800

    Made Static Medical Records Page

--- app/Public/MedicalRecords.tsx
+++ app/Public/MedicalRecords.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Pressable } from "react-native";
+import { View, Text, TextInput, Pressable, TouchableOpacity } from "react-native";
 import { useTheme } from "../../context/ThemeContext";
 import { useEffect, useState } from "react";
 import { doc, getDoc, setDoc } from "firebase/firestore";
@@ -6,6 +6,10 @@ import { auth, db } from "../../FirebaseConfig"; // use your config here
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {onAuthStateChanged} from "firebase/auth";
+import { Ionicons } from "@expo/vector-icons";
+import PlusIcon from "@/components/PlusIcon"
+import MedicalRecordsIcon from "@/components/MedicalRecordsIcon";
+
 
 export default function MedicalRecords() {
     const { theme } = useTheme();
@@ -23,11 +27,47 @@ export default function MedicalRecords() {
         return () => unsubscribe();
     }, []);
 
+    // @ts-ignore
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: theme.background }}>
-            <View className="flex-1 items-center px-4" style={{ backgroundColor: theme.background }}>
-                <Text className="text-white">{userId}</Text>
+            <View className="flex-1 items-center" style={{ backgroundColor: theme.background }}>
+                <Text className="font-bold text-2xl mb-5" style={{ color: theme.text }}>Medical Records</Text>
+                <View className="flex justify-center" style={{ backgroundColor: "#D9D9D9", borderRadius: 9, width: "80%", height: "70%" }}>
+                    <View className="flex-row justify-between border-t border-b border-gray-300 pt-6 pb-6 pl-6 pr-6">
+                        <Text className="text-black text-2xl font-bold">
+                            Medical Conditions
+                        </Text>
+
+                       <PlusIcon size={24}/>
+                    </View>
+
+                    <View className="flex-row justify-between border-t border-b border-gray-300 pt-6 pb-6 pl-6 pr-6">
+                        <Text className="text-black text-2xl font-bold">
+                            Current Medication
+                        </Text>
+
+                        <PlusIcon size={24}/>
+                    </View>
+
+                    <View className="flex-row justify-between border-t border-b border-gray-300 pt-6 pb-6 pl-6 pr-6">
+                        <Text className="text-black text-2xl font-bold">
+                            Allergies
+                        </Text>
+
+                        <PlusIcon size={24}/>
+                    </View>
+
+                    <View className="flex-row justify-between border-t border-b border-gray-300 pt-6 pb-6 pl-6 pr-6">
+                        <Text className="text-black text-2xl font-bold">
+                            Blood Type
+                        </Text>
+
+                        <PlusIcon size={24}/>
+                    </View>
+                </View>
+
+                <TouchableOpacity className="bg-[#1E88E5] mt-6 p-4 rounded-lg items-center mt-2">
+                    <Text className="text-white font-bold">Edit Info</Text>
+                </TouchableOpacity>
             </View>
-        </SafeAreaView>
     );
 }
