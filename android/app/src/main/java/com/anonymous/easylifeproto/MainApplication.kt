19093d9a9f3b22f650425776677919e78e9b1e30
Darrell Tan <darrelltwh@gmail.com>
2025-06-04 02:47:37 +0800

    Building on iOS files

--- android/app/src/main/java/com/anonymous/easylifeproto/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/easylifeproto/MainApplication.kt
@@ -1,6 +1,5 @@
 package com.anonymous.easylifeproto
 
-import com.anonymous.easylifeproto.VoskPackage
 import android.app.Application
 import android.content.res.Configuration
 
@@ -23,10 +22,9 @@ class MainApplication : Application(), ReactApplication {
         this,
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages.toMutableList()
-            packages.add(VoskPackage())
+            val packages = PackageList(this).packages
             // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
+            // packages.add(MyReactNativePackage())
             return packages
           }
 
